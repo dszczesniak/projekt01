7cd7f10f18da2485e0b952f65f2fd34e52883e23
dszczesniak <dszczesniak@poczta.pl>
2015-10-28 01:20:53 +0100

    zmiany

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 EMAIL_HOST =  'smtp.gmail.com'
 EMAIL_HOST_USER = 'testowymailemail'
-EMAIL_HOST_PASSWORD = 'haslo111'
+EMAIL_HOST_PASSWORD = 'haslo222'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 #SERVER_EAMIL = 'testowymailemail@gmail.com'
