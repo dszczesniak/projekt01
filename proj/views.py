fc6d54040b56bce4d936099cd2650376529c98e0
dszczesniak <dszczesniak@poczta.pl>
2016-03-27 17:46:06 +0200

    Ukonczone grupy2

--- proj/views.py
+++ proj/views.py
@@ -455,7 +455,8 @@ def groups(request):
 		gr = Group.objects.filter(members__name=request.user)
 
 		#definiowanie
-		p = Person.objects.get(name=request.user, id_person=cv.id)
+		for c in cv:
+			p = Person.objects.get(name=request.user, id_person=c.id)
 		gd = Group.objects.get(name=request.POST['leave'])
 		#usuwanie
 		my = Membership.objects.get(group = gd, person = p)
